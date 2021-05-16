e5908ebe8c5ca2531a5593123f88a613f4d044f9
Phil Smithies <philsmithies@gmail.com>
2021-05-16 15:04:51 +0200

    array methods 1

--- etchasketch/index.js
+++ etchasketch/index.js
@@ -1,45 +1,50 @@
 const etchContainer = document.getElementById('etchContainer')
 const newNumDivs = document.getElementById('newNumDivs')
 const submit = document.getElementById('submit')
+const resetButton = document.getElementById('resetButton');
+const etchDivs = document.getElementsByClassName('etchDiv')
+const amountOfPixels = document.getElementById('amountOfPixels')
 
 let numDivs = 16
 
-createBoard()
+createBoard(numDivs)
 
-submit.addEventListener('click', () => {
-  numDivs = newNumDivs.value
-  if (numDivs > 100){
-    alert('Number too high, please enter one below 100')
-  } 
-  createBoard()
-  newNumDivs.value = ''
-})
-
-function createBoard() {
-  etchContainer.innerHTML = ''
-  for(i = 1; i <= numDivs; i++){
+function createBoard(numDivs) {
+  etchContainer.style.gridTemplateColumns = `repeat(${numDivs}, 1fr)`;
+  etchContainer.style.gridTemplateRows = `repeat(${numDivs}, 1fr)`;
+  for(i = 1; i <= (numDivs*numDivs); i++){
+    amountOfPixels.innerText = `Currently ${numDivs} pixels per side`
     const newDiv = document.createElement('div')
     newDiv.classList.add('etchDiv')
-    newDiv.innerText = i
     etchContainer.appendChild(newDiv)
   }
+  drawDivs()
 }
 
-const etchDivs = document.getElementsByClassName('etchDiv')
+function drawDivs() {
+  Array.from(etchDivs).forEach(div => div.addEventListener('mouseover', function() {
+    console.log('you are drawing')
+    div.style.backgroundColor = 'black';
+    div.style.opacity = (parseFloat(this.style.opacity || 0) + 0.2)
+  }))
+}
 
-Array.from(etchDivs).forEach(div => div.addEventListener('mouseover', function() {
-  console.log('you are drawing')
-  div.style.backgroundColor = 'black';
-  div.style.opacity = (parseFloat(this.style.opacity || 0) + 0.1)
-}))
-
-const resetButton = document.getElementById('resetButton');
+submit.addEventListener('click', () => {
+  numDivs = parseInt(newNumDivs.value)
+  if (numDivs > 100){
+    alert('Number too high, please enter one below 100')
+  } else {
+    etchContainer.innerHTML = ''
+    createBoard(numDivs) 
+  }
+  newNumDivs.value = ''
+})
 
 resetButton.addEventListener('click', () => {
   Array.from(etchDivs).forEach(div => clearDivs(div))
 })
 
 function clearDivs(div){
-  div.style.backgroundColor = 'white';
-  div.style.opacity = 1
+  etchContainer.style.backgroundColor = 'white'
+  div.style.opacity = 0
 }
