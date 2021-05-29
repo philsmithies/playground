9fe3a5baf36978c3e2a073d4519774925d321809
“Phil <philsmithies@gmail.com>
2021-05-29 17:47:34 +0200

    html, event listeners

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,4 +1,4 @@
-function operate (a, operator, b) {
+function operate (b) {
   if (operator == '+'){
     return a + b
   } else if (operator == '-'){
@@ -8,4 +8,65 @@ function operate (a, operator, b) {
   } else if (operator == '/'){
     return (a / b).toFixed(3)
   } 
+}
+
+const evaluation = []
+
+const addition = document.getElementById('addition')
+addition.addEventListener('click', () =>{
+  evaluation.push(inputField.value, '+')
+  updateField(inputField.value, '+')
+  clearField()
+  console.log(evaluation)
+})
+
+const subtraction = document.getElementById('subtraction')
+subtraction.addEventListener('click', () =>{
+  evaluation.push(inputField.value, '-')
+  updateField(inputField.value, '-')
+  clearField()
+  console.log(evaluation)
+})
+
+const multiplication = document.getElementById('multiplication')
+multiplication.addEventListener('click', () =>{
+  evaluation.push(inputField.value, '*')
+  updateField(inputField.value, '*')
+  clearField()
+  console.log(evaluation)
+})
+
+const division = document.getElementById('division')
+division.addEventListener('click', () =>{
+  evaluation.push(inputField.value, '/')
+  updateField(inputField.value, '/')
+  clearField()
+  console.log(evaluation)
+})
+
+const inputField = document.getElementById('inputField')
+inputField.addEventListener('click', () =>{
+  console.log('inputField')
+})
+
+const equals = document.getElementById('equals')
+equals.addEventListener('click', () =>{
+  console.log(evaluation.reduce((a, b) => a + b, 0))
+})
+
+const clear = document.getElementById('clear')
+clear.addEventListener('click', () =>{
+  inputField.value = ''
+})
+
+const display = document.getElementById('calculator_display')
+const operator = document.getElementById('calculator_operator')
+
+function updateField(value, method){
+  display.innerText = value
+  operator.innerText = method
+}
+
+function clearField() {
+  inputField.value = ''
 }
