02a77b5f8df09fde6e8df92bc14d68e4e3e31c4b
“Phil <philsmithies@gmail.com>
2021-05-29 12:15:48 +0200

    find the oldest

--- js-exercises/findTheOldest/findTheOldest.js
+++ js-exercises/findTheOldest/findTheOldest.js
@@ -1,3 +1,11 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+  const date = new Date();
+  let peopleSorted = people.sort(function(a, b){
+    if (b.yearOfDeath == undefined){
+      b['yearOfDeath'] = date.getFullYear()
+    }
+    return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+  })
+  return peopleSorted[0]
 }
 module.exports = findTheOldest;
