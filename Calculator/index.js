f6f0cb7a4a1ef2dbaa4b0f524af66824721cb188
“Phil <philsmithies@gmail.com>
2021-05-31 17:24:04 +0200

    CSS

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,6 +2,17 @@ let runningTotal = 0
 let operatingElement = ''
 let firstEntry = true
 
+const display = document.getElementById('calculator_display')
+const operator = document.getElementById('calculator_operator')
+const additionKey = document.getElementById('addition')
+const subtraction = document.getElementById('subtraction')
+const multiplication = document.getElementById('multiplication')
+const division = document.getElementById('division')
+const inputField = document.getElementById('inputField')
+const equals = document.getElementById('equals')
+const clear = document.getElementById('clear')
+
+
 function operate (b, operator) {
   if (firstEntry == true){
     runningTotal = parseInt(b)
@@ -32,33 +43,28 @@ function operate (b, operator) {
 } 
 
 
-const addition = document.getElementById('addition')
-
-addition.addEventListener('click', () =>{
-  updateField(inputField.value)
-  operate(inputField.value, '+')
-  console.log(operatingElement)
-  clearField()
-  console.log(runningTotal)
-})
-
 let someFunction = function(e) {
   if (e.code === 'Digit1') updateField(1)
   else if (e.code === "Digit2") updateField(2)
-  else if (e.code === "Digit3") console.log(3)
-  else if (e.code === "Digit4") console.log(4)
-  else if (e.code === "Digit5") console.log(5)
-  else if (e.code === "Digit6") console.log(6)
-  else if (e.code === "Digit7") console.log(7)
-  else if (e.code === "Digit8") console.log(8)
-  else if (e.code === "Digit9") console.log(9)
-  else if (e.code === "Digit2") console.log(2)
+  else if (e.code === "Digit3") updateField(3)
+  else if (e.code === "Digit4") updateField(4)
+  else if (e.code === "Digit5") updateField(5)
+  else if (e.code === "Digit6") updateField(6)
+  else if (e.code === "Digit7") updateField(7)
+  else if (e.code === "Digit8") updateField(8)
+  else if (e.code === "Digit9") updateField(9)
+  else if (e.code === "Digit0") updateField(0)
+  else if (e.code === "Enter") finalValue()
+  else if (e.code === "KeyC") clearFunction()
 }
 
 // addition.addEventListener('click', someFunction, false);
 window.addEventListener('keydown', someFunction, false);
 
-const subtraction = document.getElementById('subtraction')
+additionKey.addEventListener('click', () =>{
+  addition()
+})
+
 subtraction.addEventListener('click', () =>{
   updateField(inputField.value)
   operate(inputField.value, '-')
@@ -66,7 +72,6 @@ subtraction.addEventListener('click', () =>{
   console.log(runningTotal)
 })
 
-const multiplication = document.getElementById('multiplication')
 multiplication.addEventListener('click', () =>{
   updateField(inputField.value)
   operate(inputField.value, '*')
@@ -74,7 +79,6 @@ multiplication.addEventListener('click', () =>{
   console.log(runningTotal)
 })
 
-const division = document.getElementById('division')
 division.addEventListener('click', () =>{
   updateField(inputField.value)
   operate(inputField.value, '/')
@@ -83,10 +87,25 @@ division.addEventListener('click', () =>{
   console.log(runningTotal)
 })
 
-const inputField = document.getElementById('inputField')
 
-const equals = document.getElementById('equals')
 equals.addEventListener('click', () =>{
+  finalValue()
+})
+
+clear.addEventListener('click', () =>{
+  clearFunction()
+})
+
+function updateField(value){
+  inputField.value = value
+  display.innerText = value
+}
+
+function clearField() {
+  inputField.value = ''
+}
+
+function finalValue() {
   operate(inputField.value, operatingElement)
   console.log(operatingElement)
   console.log('final number is ' + ' ' + runningTotal)
@@ -94,26 +113,22 @@ equals.addEventListener('click', () =>{
   runningTotal = 0
   clearField()
   firstEntry = true
-})
+}
 
-const clear = document.getElementById('clear')
-clear.addEventListener('click', () =>{
+function clearFunction(){
   inputField.value = ''
   runningTotal = 0
   operatingElement = ''
   updateField('', '')
   firstEntry = true
-})
-
-const display = document.getElementById('calculator_display')
-const operator = document.getElementById('calculator_operator')
-
-function updateField(value){
-  display.innerText = value
 }
 
-function clearField() {
-  inputField.value = ''
+function addition() {
+  updateField(inputField.value)
+  operate(inputField.value, '+')
+  console.log(operatingElement)
+  clearField()
+  console.log(runningTotal)
 }
 
 // each time that you press the + / - / * etc you should evaluate it and then carry on with the next thing so  
